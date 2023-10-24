d8d6ab8b950e387d51bd2bc2f975c337a705ff7f
klauth86 <kehel@yandex.ru>
2023-10-24 08:14:32 +0200

    Add empty plugin

---
+++ Plugins/EnhancedInputSequence/Source/EnhancedInputSequence/Private/EnhancedInputSequence.cpp
@@ -0,0 +1,17 @@
+// Copyright 2023 Pentangle Studio under EULA https://www.unrealengine.com/en-US/eula/unreal
+
+#include "EnhancedInputSequence.h"
+
+#define LOCTEXT_NAMESPACE "FEnhancedInputSequenceModule"
+
+void FEnhancedInputSequenceModule::StartupModule()
+{
+}
+
+void FEnhancedInputSequenceModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEnhancedInputSequenceModule, EnhancedInputSequence)
---
+++ Plugins/EnhancedInputSequence/Source/EnhancedInputSequenceEditor/Private/EnhancedInputSequenceEditor.cpp
@@ -0,0 +1,17 @@
+// Copyright 2023 Pentangle Studio under EULA https://www.unrealengine.com/en-US/eula/unreal
+
+#include "EnhancedInputSequenceEditor.h"
+
+#define LOCTEXT_NAMESPACE "FEnhancedInputSequenceEditorModule"
+
+void FEnhancedInputSequenceEditorModule::StartupModule()
+{
+}
+
+void FEnhancedInputSequenceEditorModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEnhancedInputSequenceEditorModule, EnhancedInputSequenceEditor)
