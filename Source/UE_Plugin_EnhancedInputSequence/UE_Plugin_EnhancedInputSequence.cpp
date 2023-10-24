01f2666d5d0285c99ec93360bde567e1231bc431
klauth86 <kehel@yandex.ru>
2023-10-24 07:32:03 +0200

    Add empty proj

---
+++ Source/UE_Plugin_EnhancedInputSequence/UE_Plugin_EnhancedInputSequence.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Plugin_EnhancedInputSequence.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Plugin_EnhancedInputSequence, "UE_Plugin_EnhancedInputSequence" );
