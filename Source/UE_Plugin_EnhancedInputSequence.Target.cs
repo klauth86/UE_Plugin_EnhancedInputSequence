1a1d0933da6e54392e00f0d8f5f89b893051d182
klauth86 <kehel@yandex.ru>
2026-06-16 17:08:00 +0200

    RELEASE 1.6

--- Source/UE_Plugin_EnhancedInputSequence.Target.cs
+++ Source/UE_Plugin_EnhancedInputSequence.Target.cs
@@ -8,8 +8,8 @@ public class UE_Plugin_EnhancedInputSequenceTarget : TargetRules
 	public UE_Plugin_EnhancedInputSequenceTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("UE_Plugin_EnhancedInputSequence");
 	}
 }
--- Source/UE_Plugin_EnhancedInputSequenceEditor.Target.cs
+++ Source/UE_Plugin_EnhancedInputSequenceEditor.Target.cs
@@ -8,8 +8,8 @@ public class UE_Plugin_EnhancedInputSequenceEditorTarget : TargetRules
 	public UE_Plugin_EnhancedInputSequenceEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("UE_Plugin_EnhancedInputSequence");
 	}
 }
